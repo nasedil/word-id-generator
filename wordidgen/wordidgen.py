aa63d5e6f319f8fa4c694ceb4279055f029d59aa
Eugene Petkevich <nasedil.genio.code@gmail.com>
2021-07-21 18:24:23 +0300

    Add dummy functionality

--- wordidgen/wordidgen.py
+++ wordidgen/wordidgen.py
@@ -5,10 +5,19 @@ from argparse import ArgumentParser
 
 def main():
     """Helper command-line tool for word id generator."""
-    parser = ArgumentParser(description='Word id generators',
+    parser = ArgumentParser(description='Word id generator',
                             formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-l', '--len',
+                        type=int,
+                        help='number of syllables in the word')
+    parser.add_argument('-c', '--capitalize',
+                        action='store_true',
+                        help='whether the word should be capitalized')
     args = parser.parse_args()
-    print('Хыхо')
+    word = 'хо' * args.len
+    if args.capitalize:
+        word = word.capitalize()
+    print(word)
 
 if __name__ == '__main__':
     main()
