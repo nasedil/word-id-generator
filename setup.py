f287b96c4401f63a8de30b6aae2192456d61d8e4
Eugene Petkevich <nasedil.genio.code@gmail.com>
2021-08-01 17:18:29 +0300

    Define 0.0.1 version

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 
 setup(name='wordidgen',
       packages=['wordidgen'],
+      version = '0.0.1',
       entry_points={
         'console_scripts': ['wordidgen = wordidgen.wordidgen:main']
       })
