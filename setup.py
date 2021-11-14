bd802df189f03f111c6e7943315549d43d0838ca
Eugene Petkevich <nasedil.genio.code@gmail.com>
2021-11-14 16:39:14 +0300

    Define v0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='wordidgen',
       packages=['wordidgen'],
-      version = '0.0.2',
+      version = '0.0.3',
       entry_points={
         'console_scripts': ['wordidgen = wordidgen.wordidgen:main']
       })
