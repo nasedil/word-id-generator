71f4d802a2e4cb5a5d3d18af1b10009ea7995bbf
Gene Zuelum <code@zuelum.org>
2026-02-09 07:25:37 +0100

    add random seed

--- wordidgen/wordidgen.py
+++ wordidgen/wordidgen.py
@@ -6,63 +6,65 @@ a word that corresponds to a larger number, unless "ё" and "е" are in the same
 position.  So if you need fully sortable system, you have to exclude one of
 those letters in word generator.
 """
-from argparse import ArgumentDefaultsHelpFormatter
-from argparse import ArgumentParser
-from collections import namedtuple
+
 import os
 import random
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+from collections import namedtuple
 
-CONFIG_FOLDER = 'wordidgen-a20ad162-f2cb-11eb-81a6-33a7c6316f2d'
+CONFIG_FOLDER = "wordidgen-a20ad162-f2cb-11eb-81a6-33a7c6316f2d"
 
-Consonant = namedtuple('Consonant', ['hard_letter', 'soft_letter'])
-Vowel = namedtuple('Vowel', ['letter', 'is_soft'])
+Consonant = namedtuple("Consonant", ["hard_letter", "soft_letter"])
+Vowel = namedtuple("Vowel", ["letter", "is_soft"])
 
-CONSONANTS_REGULAR = [Consonant(l, l) for l in 'бвгджзклмнпрстфхцч']
-CONSONANT_SH_hard = Consonant('ш', None)
-CONSONANT_SH_soft = Consonant(None, 'щ')
-CONSONANTS_EXTRA = [Consonant(l, l) for l in 'ґџӡҏӈҙҫԋԉԃԏ']
+CONSONANTS_REGULAR = [Consonant(l, l) for l in "бвгджзклмнпрстфхцч"]
+CONSONANT_SH_hard = Consonant("ш", None)
+CONSONANT_SH_soft = Consonant(None, "щ")
+CONSONANTS_EXTRA = [Consonant(l, l) for l in "ґџӡҏӈҙҫԋԉԃԏ"]
 CONSONANTS_EXTRA_PAIRS = [
-    Consonant('ў', 'ѱ'),
-    Consonant('ӿ', 'й'),
-    Consonant('ѣ', None),
-    Consonant('һ', 'ҩ'),
-    Consonant('ѳ', 'ғ'),
+    Consonant("ў", "ѱ"),
+    Consonant("ӿ", "й"),
+    Consonant("ѣ", None),
+    Consonant("һ", "ҩ"),
+    Consonant("ѳ", "ғ"),
 ]
-CONSONANTS_CYRILLIC = (CONSONANTS_REGULAR
-                       + [CONSONANT_SH_hard, CONSONANT_SH_soft])
-CONSONANTS_EXTCYRILLIC = (CONSONANTS_REGULAR
-                          + [CONSONANT_SH_hard, CONSONANT_SH_soft]
-                          + CONSONANTS_EXTRA + CONSONANTS_EXTRA_PAIRS)
-EASY_CONSONANTS_CYRILLIC = [Consonant(l, l) for l in 'бвгзклмнпсфхц']
+CONSONANTS_CYRILLIC = CONSONANTS_REGULAR + [CONSONANT_SH_hard, CONSONANT_SH_soft]
+CONSONANTS_EXTCYRILLIC = (
+    CONSONANTS_REGULAR
+    + [CONSONANT_SH_hard, CONSONANT_SH_soft]
+    + CONSONANTS_EXTRA
+    + CONSONANTS_EXTRA_PAIRS
+)
+EASY_CONSONANTS_CYRILLIC = [Consonant(l, l) for l in "бвгзклмнпсфхц"]
 
-VOWELS_HARD = [Vowel(l, False) for l in 'аоуыэ']
-VOWELS_SOFT = [Vowel(l, True) for l in 'еёиюя']
-VOWELS_CYRILLIC = (VOWELS_HARD[0:1] + VOWELS_SOFT[0:3]
-                   + VOWELS_HARD[1:5] + VOWELS_SOFT[3:5])
-EASY_VOWELS_CYRILLIC = (VOWELS_HARD[0:1] + VOWELS_SOFT[0:3:2]
-                        + VOWELS_HARD[1:3])
+VOWELS_HARD = [Vowel(l, False) for l in "аоуыэ"]
+VOWELS_SOFT = [Vowel(l, True) for l in "еёиюя"]
+VOWELS_CYRILLIC = (
+    VOWELS_HARD[0:1] + VOWELS_SOFT[0:3] + VOWELS_HARD[1:5] + VOWELS_SOFT[3:5]
+)
+EASY_VOWELS_CYRILLIC = VOWELS_HARD[0:1] + VOWELS_SOFT[0:3:2] + VOWELS_HARD[1:3]
 
-CONSONANTS_LATIN = [Consonant(l, l) for l in 'bcdfghjklmnpqrstvwxz']
-VOWELS_LATIN = [Vowel(l, False) for l in 'aeiouy']
+CONSONANTS_LATIN = [Consonant(l, l) for l in "bcdfghjklmnpqrstvwxz"]
+VOWELS_LATIN = [Vowel(l, False) for l in "aeiouy"]
 
-EASY_CONSONANTS_LATIN = [Consonant(l, l) for l in 'mnktp']
-EASY_VOWELS_LATIN = [Vowel(l, False) for l in 'aiu']
+EASY_CONSONANTS_LATIN = [Consonant(l, l) for l in "mnktp"]
+EASY_VOWELS_LATIN = [Vowel(l, False) for l in "aiu"]
 
 VOCABULARIES = {
-    'cyrillic': (CONSONANTS_CYRILLIC, VOWELS_CYRILLIC),
-    'extcyrillic': (CONSONANTS_EXTCYRILLIC, VOWELS_CYRILLIC),
-    'latin': (CONSONANTS_LATIN, VOWELS_LATIN),
-    'easy_latin': (EASY_CONSONANTS_LATIN, EASY_VOWELS_LATIN),
-    'easy_cyrillic': (EASY_CONSONANTS_CYRILLIC, EASY_VOWELS_CYRILLIC),
+    "cyrillic": (CONSONANTS_CYRILLIC, VOWELS_CYRILLIC),
+    "extcyrillic": (CONSONANTS_EXTCYRILLIC, VOWELS_CYRILLIC),
+    "latin": (CONSONANTS_LATIN, VOWELS_LATIN),
+    "easy_latin": (EASY_CONSONANTS_LATIN, EASY_VOWELS_LATIN),
+    "easy_cyrillic": (EASY_CONSONANTS_CYRILLIC, EASY_VOWELS_CYRILLIC),
 }
+
 
 class WordGenerator:
     """Generate word by combining syllables"""
 
-    def __init__(self,
-                 consonants=CONSONANTS_EXTCYRILLIC,
-                 vowels=VOWELS_CYRILLIC,
-                 length=4):
+    def __init__(
+        self, consonants=CONSONANTS_EXTCYRILLIC, vowels=VOWELS_CYRILLIC, length=4
+    ):
         self.consonants = consonants
         self.vowels = vowels
         self.length = length
@@ -74,11 +76,11 @@ class WordGenerator:
                 if not j.is_soft and i.hard_letter is not None:
                     self.syllables.append(i.hard_letter + j.letter)
         self.syllable_count = len(self.syllables)
-        self.count = self.syllable_count ** self.length
+        self.count = self.syllable_count**self.length
 
     def generate(self, number=None):
         """Generate a word"""
-        word = ''
+        word = ""
         if number is None:
             number = random.randrange(self.count)
         for _ in range(self.length):
@@ -87,46 +89,63 @@ class WordGenerator:
             word = self.syllables[r] + word
         return word
 
+
 def main():
     """Helper command-line tool for word id generator."""
-    parser = ArgumentParser(description='Word id generator',
-                            formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-l', '--length',
-                        type=int,
-                        default=4,
-                        help='number of syllables in the word')
-    parser.add_argument('-n', '--number',
-                        type=int,
-                        default=-1,
-                        help='number of the word (random otherwise)')
-    parser.add_argument('-t', '--title',
-                        action='store_true',
-                        help='whether the word should be capitalized')
-    parser.add_argument('-c', '--count',
-                        action='store_true',
-                        help='print number of possible words')
-    parser.add_argument('-s', '--save',
-                        action='store_true',
-                        help='remember that the word was generated')
-    parser.add_argument('-u', '--unique',
-                        action='store_true',
-                        help='do not generate remembered words again')
-    parser.add_argument('-v', '--vocabulary',
-                        default='extcyrillic',
-                        help='set of letters to use (' + ', '.join(VOCABULARIES.keys()) + ')')
+    parser = ArgumentParser(
+        description="Word id generator", formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-l", "--length", type=int, default=4, help="number of syllables in the word"
+    )
+    parser.add_argument(
+        "-n",
+        "--number",
+        type=int,
+        default=-1,
+        help="number of the word (random otherwise)",
+    )
+    parser.add_argument(
+        "-t",
+        "--title",
+        action="store_true",
+        help="whether the word should be capitalized",
+    )
+    parser.add_argument(
+        "-c", "--count", action="store_true", help="print number of possible words"
+    )
+    parser.add_argument(
+        "-s", "--save", action="store_true", help="remember that the word was generated"
+    )
+    parser.add_argument(
+        "-u",
+        "--unique",
+        action="store_true",
+        help="do not generate remembered words again",
+    )
+    parser.add_argument(
+        "-v",
+        "--vocabulary",
+        default="extcyrillic",
+        help="set of letters to use (" + ", ".join(VOCABULARIES.keys()) + ")",
+    )
+    parser.add_argument("--seed", type=int, help="Random seed")
     args = parser.parse_args()
+    if args.seed is not None:
+        random.seed(args.seed)
     consonants, vowels = VOCABULARIES[args.vocabulary]
-    word_generator = WordGenerator(length=args.length,
-                                   consonants=consonants,
-                                   vowels=vowels)
-    config_name = 'ба' + str(args.length) + '.txt'
-    config_path = os.path.join(os.path.expanduser("~"), '.config',
-                               CONFIG_FOLDER, config_name)
+    word_generator = WordGenerator(
+        length=args.length, consonants=consonants, vowels=vowels
+    )
+    config_name = "ба" + str(args.length) + ".txt"
+    config_path = os.path.join(
+        os.path.expanduser("~"), ".config", CONFIG_FOLDER, config_name
+    )
     os.makedirs(os.path.dirname(config_path), exist_ok=True)
     generated_words = []
     if args.save:
         try:
-            with open(config_path, 'rt+') as f:
+            with open(config_path, "rt+") as f:
                 generated_words = f.read().splitlines()
         except:
             pass
@@ -134,10 +153,10 @@ def main():
         print(word_generator.count)
         return
     if len(generated_words) >= word_generator.count:
-        print('All possible unique words are already generated')
+        print("All possible unique words are already generated")
         return
     while True:
-        #TODO what if all words are already generated?
+        # TODO what if all words are already generated?
         number = args.number
         if args.number == -1:
             number = None
@@ -145,16 +164,17 @@ def main():
         if word not in generated_words or not args.unique:
             break
         if args.number != -1 and args.unique:
-            print(word, 'is already generated')
+            print(word, "is already generated")
             return
     if args.save:
-        #TODO need to make thread-safe
-        #TODO use correct path on different systems
-        with open(config_path, 'at+') as f:
-            f.write(word + '\n')
+        # TODO need to make thread-safe
+        # TODO use correct path on different systems
+        with open(config_path, "at+") as f:
+            f.write(word + "\n")
     if args.title:
         word = word.capitalize()
     print(word)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
